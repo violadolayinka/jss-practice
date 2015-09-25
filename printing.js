04eb5e71b0687a6b3afcc382088664cc6420970f
violadolayinka <violadolayinka@gmail.com>
2015-09-25 00:07:24 -0700

    another commit, just completed practicing printing in JavaScript versus in Python.

---
+++ printing.js
@@ -0,0 +1,24 @@
+//This sheet will review printing statements in JavaScript versus Python
+//Python print statements will be commented out. 
+//While JavaScript statements will not be commented out. 
+
+//Python: 
+//print "Hi"
+//This returns the string "Hi" as Hi 
+
+//JavaScript: 
+console.log("Hi");
+//This returns Hi 
+
+//Python
+//print "Hello", "World"
+//This returns two strings seperated by a "," as Hello,World
+
+//JavaScript
+console.log("Hello", "World");
+
+//Python 
+//print "I love %s but not %s" % (good, bad)
+//This returns a string with two string variables filling the %s placeholers:
+// I love good but not bad
+
