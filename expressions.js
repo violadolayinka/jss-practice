5c22890ffcd5a449ef8ee3e75f89f402ecb7bc67
violadolayinka <violadolayinka@gmail.com>
2015-09-25 00:35:25 -0700

    commiting expressions review.

---
+++ expressions.js
@@ -0,0 +1,36 @@
+//This sheet will review expressions in Python and JavaScript: 
+
+//Condition: less than
+//Python: <
+//JavaScript: <
+
+//Condition: greater than
+//Python: >
+//JavaScript: >
+
+//Condition: less than or =
+//Python: <=
+//JavaScript: <=
+
+//Condition: greater than or = 
+//Python: >=
+//JavaScript: >=
+
+//Condition: equality 
+//Python: ==
+//JavaScript: ==
+
+//Condition: strict equality
+//Python: is
+//JavaScript: ===
+
+//Condition: inequality
+//Python: !=
+//JavaScript: !==
+
+//Moral of the lesson -- Use the "strict equality" and "strict inequality"
+//operators whenever possible.Javascript is particularly loose about data types, 
+//happily coercing (changing) a data from one type to another to compare it 
+//in an expression. 
+//i.e.: In Javascript, "42" == 42 (but "42" !== 42)
+
