76dfc0b14d74d411a5b6f93156592f2cecf60029
violadolayinka <violadolayinka@gmail.com>
2015-09-25 00:20:32 -0700

    commiting variables reviews

---
+++ variables.js
@@ -0,0 +1,22 @@
+//In this sheet I will review the difference in JS variables
+//versus in Python. 
+
+//Variables 
+
+//Python
+//my_num = 10
+//my_string = "Hello"
+
+//JavaScript 
+//In JS we have to options available, I can either: 
+
+//1) Declare a variable: 
+var myNum, myString;
+
+myNum = 10;
+
+myString = "Hello";
+
+//2) Or can declare & assign at once 
+var myNum = 10;
+var myString = "Hello";
